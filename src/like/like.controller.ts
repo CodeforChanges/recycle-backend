aec7009a55f22fec96f05eeebb4ca19924be41b4
최기환 <rlghks3004@gmail.com>
2024-02-03 22:12:44 +0900

    feat(like): 좋아요 create, delete 엔드포인트 생성
     - create시 이미 좋아요를 누른 게시물이라면 400 스테이터스 코드 반환.
     - create 와 delete로 좋아요를 누르거나 취소하거나를 구현하도록 함.

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { LikeService } from './like.service';
+import { CreateLikeDto } from './dto/create-like.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { Request } from 'express';
 
 @Controller('like')
 export class LikeController {
   constructor(private readonly likeService: LikeService) {}
+
+  @UseGuards(AuthGuard)
+  @Post()
+  async createOne(
+    @Body() createOneDto: CreateLikeDto,
+    @Req() request: Request,
+  ) {
+    const user_id = request['user'].user_id;
+    return await this.likeService.createOne(createOneDto, user_id);
+  }
+
+  @UseGuards(AuthGuard)
+  @Delete(':id')
+  async deleteOne(@Param('id') id) {
+    return await this.deleteOne(+id);
+  }
 }
