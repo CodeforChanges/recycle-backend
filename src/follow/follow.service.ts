5d251d9b7b9f607e1b8f0ff68acca1ac33c04501
최기환 <rlghks3004@gmail.com>
2024-02-04 10:55:10 +0900

    fix(api): 팔루우시 이미 팔로우 기록이 있으면 에러 반환

--- src/follow/follow.service.ts
+++ src/follow/follow.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { FollowServiceArgs } from './types/follow.types';
 
@@ -7,6 +7,19 @@ export class FollowService {
   constructor(private prisma: PrismaService) {}
 
   async follow({ follower_id, following_id }: FollowServiceArgs) {
+    const existingFollow = await this.prisma.follow.findFirst({
+      where: {
+        follower_id,
+        AND: {
+          following_id,
+        },
+      },
+    });
+
+    if (existingFollow) {
+      throw new BadRequestException('이미 팔로우한 계정입니다.');
+    }
+
     return await this.prisma.follow.create({
       data: {
         follower: {
