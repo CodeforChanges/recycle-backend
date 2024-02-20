3d463c252976727253cc4bf36361c5c6755ca476
YeJun, Jung <yejun614@naver.com>
2024-02-20 14:18:37 +0900

    feat: AI module 추가

---
+++ src/ai/ai.controller.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { GarbageClassificationRequestDto } from './dto/garbage-classification-request.dto';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
+import { AiService } from './ai.service';
+import { ModelRequestResponse } from './dto/model-request-response.dto';
+
+@Controller('ai')
+export class AiController {
+  constructor(private readonly aiService: AiService) {}
+
+  @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary: '쓰레기 종류 분류 모델 추론 요청',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '추론 요청 ID 반환',
+    type: ModelRequestResponse,
+  })
+  @Post('garbage')
+  async garbageClassificationRequest(
+    @Body() dto: GarbageClassificationRequestDto,
+  ) {
+    return await this.aiService.garbageClassificationRequest(dto);
+  }
+
+  @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary: '쓰레기 종류 분류 모델 결과 확인',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: '모델 추론 ID',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '추론 결환 반환',
+  })
+  @Get('garbage/:id')
+  async getGarbageClassificationResult(@Param('id') id: number) {
+    return this.aiService.getGarbageClassificationResult(id);
+  }
+}
---
+++ src/ai/ai.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import * as amqplib from 'amqplib';
+import { GarbageClassificationRequestDto } from './dto/garbage-classification-request.dto';
+import { ConfigService } from '@nestjs/config';
+import { ModelRequestResponse } from './dto/model-request-response.dto';
+
+@Injectable()
+export class AiService {
+  conn: any;
+  channel: any;
+  memory = {};
+
+  constructor(private readonly configService: ConfigService) {
+    this.initMQ();
+  }
+
+  private async initMQ() {
+    const url = this.configService.get('RECYCLE_AI_MQ_URL');
+
+    this.conn = await amqplib.connect(url);
+    this.channel = await this.conn.createChannel();
+
+    this.channel.consume('garbage_classification_results', (body) => {
+      const response = JSON.parse(body);
+      this.memory[response['id']] = response;
+    });
+  }
+
+  async garbageClassificationRequest(
+    dto: GarbageClassificationRequestDto,
+  ): Promise<ModelRequestResponse> {
+    const datetime = Date.now();
+
+    const body = {
+      id: datetime,
+      data: dto.imageUrl,
+    };
+
+    this.channel.sendToQueue(
+      'garbage_classification_requests',
+      JSON.stringify(body),
+    );
+
+    const request = new ModelRequestResponse();
+    request.id = body.id;
+
+    return request;
+  }
+
+  getGarbageClassificationResult(id: number) {
+    return this.memory[id] ?? undefined;
+  }
+}
