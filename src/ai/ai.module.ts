68bff9d10b4baffa7000b8e728f4e521b7bc77be
YeJun, Jung <yejun614@naver.com>
2024-02-20 14:34:24 +0900

    fix: 의존성 문제 해결

--- src/ai/ai.module.ts
+++ src/ai/ai.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AiController } from './ai.controller';
 import { AiService } from './ai.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   controllers: [AiController],
   providers: [AiService],
 })
