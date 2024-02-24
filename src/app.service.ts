14678922cc4831b6f12f1e1a027eeabcab4025c5
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-02-24 16:04:31 +0900

    CI/CD test 2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Test for CI/CD Hello Code for Changes!';
+    return 'Test for CI/CD Hello Code for Changes! Second try';
   }
 }
